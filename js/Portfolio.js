b4a3959d5ea5d66910711d4797cea440395cc2c2
ashishkr-18 <67505020+ashishkr-18@users.noreply.github.com>
2020-09-27 00:58:38 +0530

    My portfolio website

---
+++ js/Portfolio.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+
+
+    //Slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    // Toggle menu/navbar script
+    $('.menu-button').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-button i').toggleClass("active");
+    });
+
+
+    //Typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Developer","Programmer","Designer","Student","Learner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer","Programmer","Designer","Student","Learner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    })
+});
